25aae4eac5761f4bc423317df9e834148a0d28d2
shubhamdarwante2301 <shubhamdarwante2301@gmail.com>
2021-12-21 17:21:24 +0530

    traversing-linked-list-using-first-node

---
+++ 6.linked list/traversing-linked-list-using-first-node.cpp
@@ -0,0 +1,41 @@
+//we have a first node off linked list
+//we need to print the data of all nodes in linked list
+
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *head)
+{
+    Node *curr = head;
+    while(curr != NULL)
+    {
+        cout<<curr->data<<endl;
+        curr = curr->next;
+    }
+}
+int main()
+{
+    Node *head  = new Node(10);
+    Node *temp1  = new Node(20);
+    Node *temp2  = new Node(30);
+    Node *temp3  = new Node(40);
+    Node *temp4  = new Node(50);
+    
+    head->next = temp1;
+    temp1->next = temp2;
+    temp2->next = temp3;
+    temp3->next = temp4;
+
+    printList(head);
+    return 0;
+}
