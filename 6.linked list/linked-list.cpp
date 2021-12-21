c9a98ba27ca29d20558f2e5e3aa1a3f921077547
shubhamdarwante2301 <shubhamdarwante2301@gmail.com>
2021-12-21 16:58:47 +0530

    6.linked list

---
+++ 6.linked list/linked-list.cpp
@@ -0,0 +1,27 @@
+//first linked list programm wich create 3 node in linked list
+
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+
+    head->next = temp1;
+    temp1->next = temp2;
+
+    cout<<head->data<<"-->"<<temp1->data<<"-->"<<temp2->data;
+    return 0;
+}
