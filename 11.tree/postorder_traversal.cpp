1053378583f1bea679d810e45bb3db7cdb031054
shubhamdarwante2301 <shubhamdarwante2301@gmail.com>
2022-03-23 11:44:43 +0530

    postorder_traversal

---
+++ 11.tree/postorder_traversal.cpp
@@ -0,0 +1,37 @@
+//postorder traversal of binary tree
+//postorder(left, right, root)
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left, *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+void postorder(Node *root)
+{
+    if(root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        cout<<(root->key)<<" ";
+    }
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->right->left = new Node(40);
+    root->right->right = new Node(50);
+
+    postorder(root);
+    return 0;
+}
